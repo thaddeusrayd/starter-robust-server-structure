d1b776db820614068f3879412d76617349579566
thaddeusrayd <thaddeusrayd@gmail.com>
2021-09-30 13:00:37 -0500

    create API endpoint

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 
 // TODO: Follow instructions in the checkpoint to implement ths API.
+const flips = require("./data/flips-data");
+
+app.use("/flips", (req, res) => {
+  res.json({ data: flips });
+});
 
 // Not found handler
 app.use((request, response, next) => {
