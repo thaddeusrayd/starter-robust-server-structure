d49fe312ace7ffd39452bbc6cac17812b8e6d8b2
thaddeusrayd <thaddeusrayd@gmail.com>
2021-09-30 15:28:40 -0500

    finishing touches

--- src/app.js
+++ src/app.js
@@ -5,6 +5,17 @@ const app = express();
 const flips = require("./data/flips-data");
 const counts = require("./data/counts-data");
 
+app.use("/counts/:countId", (req, res, next) => {
+  const { countId } = req.params;
+  const foundCount = counts[countId];
+
+  if (foundCount === undefined) {
+    next(`Count id not found: ${countId}`);
+  } else {
+    res.json({ data: foundCount });
+  }
+});
+
 app.use("/counts", (req, res) => {
   res.json({ data: counts });
 });
