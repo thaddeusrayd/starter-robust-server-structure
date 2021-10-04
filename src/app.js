cafaed39921ce3ad8c2c3607d5798d3b31cc502e
thaddeusrayd <thaddeusrayd@gmail.com>
2021-10-04 14:19:05 -0500

    add controller and router for counts

--- src/app.js
+++ src/app.js
@@ -1,30 +1,12 @@
 const express = require("express");
+const countsRouter = require("./counts/counts.router");
 const flipsRouter = require("./flips/flips.router");
-
 const app = express();
-
-// TODO: Follow instructions in the checkpoint to implement ths API.
-const flips = require("./data/flips-data");
-const counts = require("./data/counts-data");
 
 app.use(express.json());
 
-app.use("/counts/:countId", (req, res, next) => {
-  const { countId } = req.params;
-  const foundCount = counts[countId];
-
-  if (foundCount === undefined) {
-    next({ status: 404, message: `Count id not found: ${countId}` });
-  } else {
-    res.json({ data: foundCount });
-  }
-});
-
-app.use("/counts", (req, res) => {
-  res.json({ data: counts });
-});
-
-app.use("/flips", flipsRouter); // Note: app.use
+app.use("/counts", countsRouter);
+app.use("/flips", flipsRouter);
 
 // Not found handler
 app.use((request, response, next) => {
