dea9dbda0b22e90d5a29cc860f2814001d2fde03
thaddeusrayd <thaddeusrayd@gmail.com>
2021-10-04 14:28:03 -0500

    experiment with modularity

--- src/counts/counts.router.js
+++ src/counts/counts.router.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const controller = require("./counts.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
+const flipsRouter = require("../flips/flips.router");
+
+router.use("/:countId/flips", flipsRouter);
 
 router.route("/:countId").get(controller.read).all(methodNotAllowed);
 
