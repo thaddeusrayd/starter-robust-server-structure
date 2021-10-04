601dcf66c2ed5f0dc2363986bec6f67d40046836
thaddeusrayd <thaddeusrayd@gmail.com>
2021-10-04 11:49:52 -0500

    finish practice

--- src/app.js
+++ src/app.js
@@ -43,13 +43,17 @@ let lastFlipId = flips.reduce((maxId, flip) => Math.max(maxId, flip.id), 0);
 
 app.post("/flips", (req, res, next) => {
   const { data: { result } = {} } = req.body;
-  const newFlip = {
-    id: ++lastFlipId, // Increment last ID, then assign as the current ID
-    result,
-  };
-  flips.push(newFlip);
-  counts[result] = counts[result] + 1; // Increment the counts
-  res.status(201).json({ data: newFlip });
+  if (result) {
+    const newFlip = {
+      id: ++lastFlipId, // Increment last ID, then assign as the current ID
+      result,
+    };
+    flips.push(newFlip);
+    counts[result] = counts[result] + 1; // Increment the counts
+    res.status(201).json({ data: newFlip });
+  } else {
+    res.sendStatus(400);
+  }
 });
 
 // Not found handler
